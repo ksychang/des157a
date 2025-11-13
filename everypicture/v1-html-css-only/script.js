e76bfe3aabbbceba05b58fce33c641221b610468
Kaitlyn Chang <ksychang@ucdavis.edu>
2025-11-13 05:42:44 -0800

    everypicture v1 last updates

---
+++ everypicture/v1-html-css-only/script.js
@@ -0,0 +1,26 @@
+
+/*
+    SOME IDEAS:
+    * Change background color on hover over 'space', 'time', or 'being'
+    * Image trail when cursor moves within those sections
+    * On click, hide sections
+    * Narrative - only reveal next section after first was explored. All available after 'being'
+    
+    * Space:
+        * Letters expand on hover over header (return home on click)
+        * Letters get pushed by cursor
+        * Image carousel, pause on hover + caption on click
+        * Background color = blues
+    
+    * Time:
+        * Letters slightly expand and disappear on hover over header (return home on click)
+        * Images arranged in a row, trigger fade out + caption fade in on hover (caption fades out)
+        * OR fade to sepia filter
+        * Background color = greens
+    
+    * Being:
+        * Letters 'breathe' on hover (return home on click)
+        * Centered image, show drag cursor on hover
+        * As image is dragged in different areas on the screen, change image
+        * Background color = reds
+*/
